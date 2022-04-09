b5e27200b2a37abcdf0b5c05f0e4b780c93d5958
Patrik Novotny <novotpat@gmail.com>
2022-04-09 19:47:34 +0200

    My first project

--- main2.py
+++ main2.py
@@ -90,39 +90,51 @@ if int(text_number) in range(1, 4):
     print(f"There are {number_of_words} words in the selected text.")
 
 # Number of capitalised words
+    big_letters = []
+    for letters in cleaned_words:
+        if letters.istitle():
+           big_letters.append(letters)
+    print(f"There are {len(big_letters)} titlecase words.")
     #    velka_pismena = sum(map(str.istitle, text.split()))
 
-    big_letters = []
+# Number of uppercase words
     upper_words = []
+    for words in cleaned_words:
+        if words.isupper() and not words.startswith("30N"):
+            upper_words.append(words)
+    print(f"There are {len(upper_words)} uppercase words.")
+
+# Number of lowercase words
     lower_words = []
+    for words in cleaned_words:
+        if words.islower():
+            lower_words.append(words)
+    print(f"There are {len(lower_words)} lowercase words.")
+
+# Number of figures
     summary_figures = []
+    for figures in cleaned_words:
+        if figures.isnumeric():
+            summary_figures.append(figures)
+    print(f"There are {len(summary_figures)} numeric strings.")
+
+# The sum of numbers
     summary_numbers = []
+    for figures in summary_figures:
+        i_numbers = int(figures)
+        summary_numbers.append(i_numbers)
+    print(f"The sum of all the numbers {sum(summary_numbers)}")
+    print(separator,f"LEN|{'OCCURENCES:':^17}|NR.", separator, sep="\n")
+
+# Chart
     length_words = {}
-    for letters, u_words, l_words, numbers, sum_numbers, len_words in cleaned_words:
-        if letters.istitle() == True:
-            big_letters.append(letters)
-        print(f"There are {len(big_letters)} titlecase words.")
-        if u_words.isupper() == True and not u_words.startswith("30N"):
-            upper_words.append(u_words)
-        print(f"There are {len(upper_words)} uppercase words.")
-        if l_words.islower() == True:
-            lower_words.append(l_words)
-        print(f"There are {len(lower_words)} lowercase words.")
-        if numbers.isnumeric() == True:
-            summary_figures.append(numbers)
-        print(f"There are {len(summary_figures)} numeric strings.")
-        if sum_numbers.isnumeric() == True:
-            i_numbers = int(sum_numbers)
-            summary_numbers.append(i_numbers)
-        print(f"The sum of all the numbers {sum(summary_numbers)}")
-        length = len(len_words)
+    for word in cleaned_words:
+        length = len(word)
         if length not in length_words:
             length_words[length] = 1
         else:
             length_words[length] = length_words[length] + 1
         sor_keys = sorted(length_words.keys())
-
-# Chart
-    print(separator,f"LEN|{'OCCURENCES:':^17}|NR.", separator, sep="\n")
     for prvek in sor_keys:
         print(f"{prvek: >3}|{'*' * length_words.get(prvek):<17}|{length_words.get(prvek)}")
+
